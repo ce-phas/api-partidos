778d34d67113e63e632a1c45101490faa2b82017
Pedro Amorim <amorimp@pm.me>
2021-12-26 19:54:03 -0300

    start project

---
+++ src/test/java/uol/compass/partidos/PartidosApplicationTests.java
@@ -0,0 +1,13 @@
+package uol.compass.partidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
