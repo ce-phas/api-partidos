7c6b79383f0580228181e174d8fb61de1e47fd4b
Pedro Amorim <amorimp@pm.me>
2021-12-27 20:21:16 -0300

    initial controller version for '/associados'

--- src/main/java/uol/compass/partidos/controller/AssociadoController.java
+++ src/main/java/uol/compass/partidos/controller/AssociadoController.java
@@ -1,9 +1,39 @@
 package uol.compass.partidos.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uol.compass.partidos.dto.AssociadoDTO;
+import uol.compass.partidos.dto.form.AssociadoFormDTO;
+import uol.compass.partidos.service.AssociadoService;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @RestController
 @RequestMapping("/associados")
 public class AssociadoController {
+
+    @Autowired
+    private AssociadoService service;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<AssociadoDTO> saveAssociado(@RequestBody AssociadoFormDTO body) {
+        AssociadoDTO associado = this.service.save(body);
+        return ResponseEntity.ok(associado);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AssociadoDTO>> getAssociados() {
+        List<AssociadoDTO> associados = this.service.getAssociados();
+        return ResponseEntity.ok(associados);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AssociadoDTO> searchAssociado(@PathVariable Long id) {
+        AssociadoDTO associado = this.service.searchAssociado(id);
+        return ResponseEntity.ok(associado);
+    }
+
 }
